c00b89930db32195e53158ea0d65ab8af0dc38a1
aditya <adityasu12@gmail.com>
2021-01-19 23:19:05 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path: 'play',
+    component: PlayComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
