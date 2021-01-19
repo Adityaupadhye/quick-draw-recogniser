35e0db3f45f62ab273bd5c1b532940dc45c6c7e1
aditya <adityasu12@gmail.com>
2021-01-19 22:19:02 +0530

    created components for 3 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
