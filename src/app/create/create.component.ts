a641d15f5b0c5927cb8e6945a8e21843ec7de32e
aditya <adityasu12@gmail.com>
2021-01-21 14:28:45 +0530

    implemented clear canvas in create page

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,3 +1,4 @@
+import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+  @ViewChild('clear')public clearbtn?:ElementRef;
+  height:number=0
+  width:number=0
+  context?:CanvasRenderingContext2D
+
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    const btncls = this.clearbtn?.nativeElement;
+  }
+
+  view(context: CanvasRenderingContext2D){
+    console.log("in play= ",context);
+    this.context=context;
+    //console.log("h=",height,"w=",width);
+    
+    //context.clearRect(0,0,0,0);
+  }
+
+  getW(wdth:number){
+    console.log("width in play=",wdth);
+    this.width=wdth;
+  }
+
+  getH(ht:number){
+    console.log("height in play=",ht);
+    this.height=ht;
+  }
+
+  cls(){
+    console.log("cls clicked");
+    
+    this.context?.clearRect(0,0,this.width, this.height);
   }
 
 }
